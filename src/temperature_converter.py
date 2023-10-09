bdf18e3143db9817e2642eb2d7028066f5972000
Aminul-97 <amin147570bd@gmail.com>
2023-10-10 01:38:13 +0600

    Update Code

--- src/temperature_converter.py
+++ src/temperature_converter.py
@@ -4,3 +4,10 @@ def celsius_to_fahrenheit(celsius: float):
     Formula: (Celsius * 9/5) + 32
     """
     return (celsius * 9 / 5) + 32
+
+def fahrenheit_to_celsius(fahrenheit: float):
+    """
+    Convert temperature from Fahrenheit to Celsius.
+    Formula: (Celsius * 9/5) + 32
+    """
+    return ( ( fahrenheit - 32 ) * 5 ) / 9
