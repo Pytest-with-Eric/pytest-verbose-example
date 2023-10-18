c8302291fc65e67d7b1861f93075a9287de16bd2
ericsalesdeandrade <sdaeric19@gmail.com>
2023-10-18 12:15:54 +0800

    Formatting

--- src/play_with_numbers.py
+++ src/play_with_numbers.py
@@ -1,20 +1,31 @@
-def smallest_number(num_list: list):
+def smallest_number(num_list: list) -> int:
     """
     Finds the smallest number from a list.
+
+    :param num_list: list of numbers
+    :return: smallest number from the list
     """
     num_list.sort()
     return num_list[0]
 
-def highest_number(num_list: list):
+
+def highest_number(num_list: list) -> int:
     """
     Finds the highest number from a list.
+
+    :param num_list: list of numbers
+    :return: highest number from the list
     """
     findmax = max(num_list)
     return findmax
 
-def number_to_string(str_len: int):
+
+def number_to_string(str_len: int) -> dict:
     """
-    Converts number to string.
+    Creates a dictionary of numbers from 0 to str_len.
+
+    :param str_len: length of the dictionary
+    :return: dictionary of numbers from 0 to str_len
     """
     num_to_text = {str(x): x for x in range(str_len)}
     return num_to_text
--- test/test_play_with_numbers.py
+++ test/test_play_with_numbers.py
@@ -1,13 +1,16 @@
 from src.play_with_numbers import smallest_number, highest_number, number_to_string
 
+
 # Tests the smallest_number() function
 def test_smallest_number():
     assert smallest_number([10, 5, 9]) == 5
+
 
 # Tests the highest_number() function
 def test_highest_number():
     assert highest_number([10, 5, 9]) == 10
 
+
 # Tests the number_to_string() function
-def test_number_to_text():
+def test_number_to_string():
     assert number_to_string(7) == number_to_string(5)
